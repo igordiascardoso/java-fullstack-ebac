75e8f2f5c41cfaee96efe7b58715d984bc717aee
igordiascardoso <129440416+igordiascardoso@users.noreply.github.com>
2024-05-09 15:38:52 -0300

    Add files via upload

---
+++ BACK-END/Módulo 6/exercicioAcorda.java
@@ -0,0 +1,7 @@
+public static void main (String[] args) {
+    System.out.print("Abre olho");
+    System.out.print("tira coberta");
+    System.out.print("Levanta");
+    System.out.print("Escova dente");
+    System.out.print("Come café da manhã");
+}
