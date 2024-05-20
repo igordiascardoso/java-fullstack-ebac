aeef98921da1ef07c244f8f6ba9d102852351fd7
igordiascardoso <igordiascardoso@gmail.com>
2024-05-20 20:55:22 -0300

    Exercicio do módulo 8, receber 4 notas e apresentar a média

---
+++ BACK-END/Módulo 8/tarefa-calculo-medio/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int a,b,c,d,notaMedia;
+
+        Scanner read = new Scanner(System.in);
+
+
+        System.out.println("Insira sua primeira nota:");
+        a = read.nextInt();
+
+        System.out.println("Insira sua segunda nota:");
+        b = read.nextInt();
+
+        System.out.println("Insira sua terceira nota:");
+        c = read.nextInt();
+
+        System.out.println("Insira sua quarta nota:");
+        d = read.nextInt();
+
+        notaMedia = (a+b+c+d)/4;
+
+        System.out.println("Sua média é: "+notaMedia);
+    }
+}
