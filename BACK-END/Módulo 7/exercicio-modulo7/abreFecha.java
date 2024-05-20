cd704bad0abd8725ff23cdfb59d861365751558d
igordiascardoso <igordiascardoso@gmail.com>
2024-05-20 16:18:08 -0300

    Atividade modulo 7 classes e comentários

---
+++ BACK-END/Módulo 7/exercicio-modulo7/abreFecha.java
@@ -0,0 +1,28 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	    int portaAberta=1;
+	    int portaFechada=0;
+	    
+	    System.out.print("Porta Aberta");
+	    
+	    if (portaAberta==1){
+	        portaFechada=1;
+	        
+	        System.out.print("Porta Fechada");
+	    }
+	    
+	    
+	    
+		
+	}
+}
