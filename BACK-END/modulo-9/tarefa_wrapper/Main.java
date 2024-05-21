17149af6bd215d2914d5204bcabbb89a9b3f9a64
igordiascardoso <igordiascardoso@gmail.com>
2024-05-20 23:31:59 -0300

    Exercicio modulo 9, alteração

--- BACK-END/modulo-9/tarefa_wrapper/Main.java
+++ BACK-END/modulo-9/tarefa_wrapper/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         long num1 = 22222222222222l;
-        int num2 = (int) num1;
+        Long num2 = (Long) num1;
 
         System.out.println(num2);
 
