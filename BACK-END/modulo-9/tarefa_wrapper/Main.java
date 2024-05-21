c10009e97e0afcc16e67fee865bcaad896e1b59a
igordiascardoso <igordiascardoso@gmail.com>
2024-05-20 23:05:33 -0300

    Criação de Casting, exercício módulo 9

---
+++ BACK-END/modulo-9/tarefa_wrapper/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        long num1 = 22222222222222l;
+        int num2 = (int) num1;
+
+        System.out.println(num2);
+
+
+    }
+}
