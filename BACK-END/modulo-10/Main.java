4b23e04d11c84d56aaf89163f32fe5063bc3718e
igordiascardoso <igordiascardoso@gmail.com>
2024-05-20 23:16:01 -0300

    Exercicio do módulo 10, uso de condicionais

---
+++ BACK-END/modulo-10/Main.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int a,b,c,d,notaMedia;
+
+        Scanner read = new Scanner(System.in);
+
+
+        System.out.println("Insira sua primeira nota:");
+        a = read.nextInt();
+
+        System.out.println("Insira sua segunda nota:");
+        b = read.nextInt();
+
+        System.out.println("Insira sua terceira nota:");
+        c = read.nextInt();
+
+        System.out.println("Insira sua quarta nota:");
+        d = read.nextInt();
+
+        notaMedia=(a+b+c+d)/4;
+
+        if (notaMedia >= 7 ){
+            System.out.println("Aprovado");
+        } else {
+            System.out.println("Reprovado");
+        }
+
+
+
+     
+    }
+}
