7ccb26f6c5f9aa7ab8956b435ab6defacbb73fab
igordiascardoso <igordiascardoso@gmail.com>
2024-05-24 23:30:23 -0300

    update para exercicio de generic Modulo 17

--- BACK-END/modulo-17/generic/Main.java
+++ BACK-END/modulo-17/generic/Main.java
@@ -1,65 +1,69 @@
-abstract class Carro{
+import java.util.ArrayList;
+import java.util.List;
+
+abstract class Carro {
     String marca;
     int velocidade;
 
-        public Carro(String marca, int velocidade){
-            this.marca=marca;
-            this.velocidade=velocidade;
-        }
+    public Carro(String marca, int velocidade) {
+        this.marca = marca;
+        this.velocidade = velocidade;
+    }
 
-        public abstract void mostramarcaeVelocidade();
+    public abstract void mostramarcaeVelocidade();
 }
 
+class Honda extends Carro {
+    boolean garantia;
 
-
-class Honda extends Carro{
-    static boolean garantia;
-
-    public Honda (boolean garantia, String marca, int velocidade){
+    public Honda(boolean garantia, String marca, int velocidade) {
         super(marca, velocidade);
-        this.garantia=garantia;
-
+        this.garantia = garantia;
     }
 
     @Override
-    public void mostramarcaeVelocidade(){
-        System.out.println("Marca : "+marca);
-        System.out.println("Velocidade: " +velocidade);
-        if (garantia == true){
-            System.out.println("Carro possui garantira infinita.");
+    public void mostramarcaeVelocidade() {
+        System.out.println("Marca: " + marca);
+        System.out.println("Velocidade: " + velocidade);
+        if (garantia) {
+            System.out.println("Carro possui garantia infinita.");
         } else {
-            System.out.println("Carro não possui gasolina");
-        }
+            System.out.println("Carro não possui garantia.");
         }
     }
+}
 
-class Nissan extends Carro{
-    static boolean gasolina;
+class Nissan extends Carro {
+    boolean gasolina;
 
-    public Nissan(boolean garantia, String marca, int velocidade){
+    public Nissan(boolean gasolina, String marca, int velocidade) {
         super(marca, velocidade);
         this.gasolina = gasolina;
-
     }
 
     @Override
-    public void mostramarcaeVelocidade(){
-        System.out.println("Marca: "+marca);
-        System.out.println("Velocidade limite: " +velocidade);
-        if (gasolina == true){
+    public void mostramarcaeVelocidade() {
+        System.out.println("Marca: " + marca);
+        System.out.println("Velocidade limite: " + velocidade);
+        if (gasolina) {
             System.out.println("Carro possui gasolina infinita.");
         } else {
-            System.out.println("Carro não tem gasolina gratis");
+            System.out.println("Carro não tem gasolina grátis.");
         }
     }
 }
 
 public class Main {
-
     public static void main(String[] args) {
-        Nissan carro1 = new Nissan(true,"Nissan",330);
-        carro1.mostramarcaeVelocidade();
-        Honda carro2 = new Honda(true,"HonDA",330);
+        List<Carro> carros = new ArrayList<>();
+        carros.add(new Nissan(true, "Nissan", 330));
+        carros.add(new Honda(true, "Honda", 330));
+        carros.add(new Nissan(false, "Nissan", 280));
+        carros.add(new Honda(false, "Honda", 250));
+
+        for (Carro carro : carros) {
+            carro.mostramarcaeVelocidade();
+            System.out.println();
+        }
     }
 }
-
